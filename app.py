c0dd207ec59135be970dacbda12f40b30dd23c2b
Ashish Raj <ashish1195.raj@gmail.com>
2025-05-26 14:06:35 +0530

    notebook updated and app.py modified

---
+++ app.py
@@ -0,0 +1,56 @@
+import sys
+import streamlit as st
+import pandas as pd
+import pickle
+import warnings
+
+
+def recommend(movie):
+    movie_index= movies[movies['title']== movie].index[0]
+    distances=similarity[movie_index]
+    movies_list=sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
+    
+    recommend_movies=[]
+    for i in movies_list:
+        movie_id=i[0]
+        #fetch poster from api
+        
+        recommend_movies.append(movies.iloc[i[0]].title)
+    return recommend_movies
+    
+   
+        
+
+movies_dict=pickle.load(open('movies_dict.pkl','rb'))
+# movies_list=movies_list['title'].values
+movies=pd.DataFrame(movies_dict)
+
+similarity=pickle.load(open('similarity.pkl','rb'))
+
+st.title('Movie Recommender System')
+
+
+selected_movie_name = st.selectbox(
+    "Select a movie to get recommendations:",
+    movies['title'].values
+)
+
+
+if st.button("Recommend"):
+    recommendations= recommend(selected_movie_name)
+    for i in recommendations:
+        st.write(i)
+# else:
+#     st.write("Goodbye")
+
+# if st.button("Aloha", type="tertiary"):
+#     st.write("Ciao")
+
+
+
+# st.write("You selected:", option)
+
+# import sys
+# print("warnings" in sys.modules)
+# print(movies_dict)
+
